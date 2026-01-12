6c299d6e3de142abe91774b9b99a2f08ee4ab6da
mlnawsdevops <mlnpublicact365@gmail.com>
2026-01-12 16:54:07 +0530

    terraform-aws-security-group-child

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "expense" {
+    name = local.sg_final_name
+    description = "allow all traffic"
+    vpc_id = var.vpc_id
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    } 
+
+    tags = merge(
+        var.common_tags,
+        var.sg_tags,
+        {
+            Name = local.sg_final_name
+        }
+    )
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+
+}
+
+variable "environment" {
+
+}
+
+variable "sg_name" {
+    type = string
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "sg_tags" {
+    default = {}
+}
