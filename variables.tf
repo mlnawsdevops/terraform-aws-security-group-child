d264b737e52401f92d39fc39b589ef63bb96b055
mlnawsdevops <mlnpublicact365@gmail.com>
2026-01-17 09:20:44 +0530

    securitygroup

--- variables.tf
+++ variables.tf
@@ -1,23 +1,26 @@
 variable "project_name" {
-
+    #variable must pass by user
 }
 
 variable "environment" {
-
+    #variable must pass by user
 }
 
 variable "sg_name" {
     type = string
+    #variable must pass by user
 }
 
 variable "vpc_id" {
-  
+    #variable must pass by user
 }
 
 variable "common_tags" {
     default = {}
+    #variable may be pass by user
 }
 
 variable "sg_tags" {
     default = {}
+    #variable may be pass by user
 }
